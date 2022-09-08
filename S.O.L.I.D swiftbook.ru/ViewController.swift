3343991622e6e1260dbb1efecb9bc63c0d8e8144
Aleksei Bodrov <bellied.strut_0v@icloud.com>
2022-09-08 17:34:22 +0300

    Add  DataStore

--- S.O.L.I.D swiftbook.ru/ViewController.swift
+++ S.O.L.I.D swiftbook.ru/ViewController.swift
@@ -1,19 +1,101 @@
-//
-//  ViewController.swift
-//  S.O.L.I.D swiftbook.ru
-//
-//  Created by Алексей on 08.09.2022.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    let dataStore = DataStore()
+
+    let screenSize = UIScreen.main.bounds
+
+    lazy var textLable: UILabel = {
+        let lable = UILabel()
+        lable.frame = CGRect(x: screenSize.width / 2 - 100, y: 50, width: 200, height: 50)
+        lable.font = .systemFont(ofSize: 36, weight: .thin)
+        lable.text = "Enter name"
+        lable.textColor = .white
+        lable.textAlignment = .left
+        lable.backgroundColor = .white.withAlphaComponent(0.1)
+        return lable
+    }()
+
+    lazy var myTectField: UITextField = {
+        let textField = UITextField()
+        textField.frame = CGRect(x: screenSize.width / 2 - 100, y: 100, width: 200, height: 50)
+        textField.backgroundColor = .white.withAlphaComponent(0.1)
+        textField.font = .systemFont(ofSize: 36, weight: .thin)
+        textField.tintColor = .blue
+        textField.textColor = .white
+        textField.keyboardAppearance = .dark
+        textField.keyboardType = .default
+        textField.delegate = self
+        return textField
+    }()
+    lazy var textLableSave: UILabel = {
+        let lable = UILabel()
+        lable.frame = CGRect(x: screenSize.width / 2 - 100, y: 150, width: 200, height: 50)
+        lable.font = .systemFont(ofSize: 36, weight: .thin)
+        lable.textColor = .white
+        lable.textAlignment = .left
+        lable.backgroundColor = .white.withAlphaComponent(0.1)
+        return lable
+    }()
+    lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: screenSize.width / 2 - 100, y: 250, width: 200, height: 50)
+        button.setTitle("Save", for: .normal)
+        button.setTitleColor(UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 1.0) , for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        button.backgroundColor = .green
+        button.addTarget(self, action: #selector(saveButtonTapped) , for: .touchUpInside)
+//        button.addTarget(self, action: #selector(touchUp), for: .touchUpOutside)
+
+        button.layer.shadowColor = UIColor.red.withAlphaComponent(0.9).cgColor
+        button.layer.shadowOpacity = 1
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = screenSize.height / 15 / 2
+        button.layer.cornerRadius = screenSize.height / 15 / 3
+        button.layer.masksToBounds = true
+
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        [textLableSave,
+         myTectField,
+         textLable,
+         saveButton].forEach {
+            view.addSubview($0)
+        }
     }
 
+    // MARK: - Business logic
 
+    func changeName() {
+        guard let name = textLabel.text, name != "" else {
+            showAlert()
+            return
+        }
+        
+        dataStore.savenameInCache(name: name)
+    }
+
+    //MARK: - User interface
+
+    func showAlert() {
+        let alert = UIAlertController(title: "WARNING", message: "Your name can't be empty.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Okey", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+
+        textLableSave.text = textField.text
+        return true
+    }
+    
+    @objc func saveButtonTapped(_ sender: Any) {
+        changeName()
+    }
 }
-
