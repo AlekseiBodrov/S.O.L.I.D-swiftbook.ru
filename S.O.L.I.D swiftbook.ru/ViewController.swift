7269478cefb1e1f922c56cc6c56dba098d2bed5f
Aleksei Bodrov <bellied.strut_0v@icloud.com>
2022-09-08 16:44:06 +0300

    Initial Commit

---
+++ S.O.L.I.D swiftbook.ru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  S.O.L.I.D swiftbook.ru
+//
+//  Created by Алексей on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
